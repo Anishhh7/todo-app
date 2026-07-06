c3d923751947a7438615b3854e3ae05783af34e7
Anishh <pokharelanish099@gmail.com>
2026-07-06 17:31:39 +0545

    fix user shchema in name

--- models/userModel.js
+++ models/userModel.js
@@ -1,82 +1,72 @@
-const crypto= require('crypto')
-const mongoose = require('mongoose');
-const validator = require('validator')
-const bcrypt= require('bcryptjs')
-
-
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A name is Mandatory']
+  name: {
+    type: String
+    // required: [true, 'A name is Mandatory']
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please enter a email id"],
+    unique: true,
+    lowerCase: true,
+    validate: [validator.isEmail, "please enter a valid email id"]
+  },
+
+  role: {
+    type: String,
+    enum: {
+      values: ["user", "admin", "validator"]
     },
 
-    email: {
-        type: String,
-        required: [true, 'Please enter a email id'],
-        unique: true,
-        lowerCase: true,
-        validate: [
-            validator.isEmail,
-            'please enter a valid email id'
-        ]
-        
-    },
+    default: "user"
+  },
 
-    role: {
-        type: String,
-        enum: {
-            values: ['user', 'admin', 'validator']
-        },
+  password: {
+    type: String,
+    minLength: [8, "password must have 8 Character."],
+    required: [true, "please enter your password"],
+    select: false
+  },
 
-        default: 'user'
-    },
+  passwordConfirm: {
+    type: String,
+    require: [true, "please confirm your password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Password didnt match"
+    }
+  },
 
+  passwordChangedAt: Date,
 
-    password: {
-        type: String,
-        minLength: [8, 'password must have 8 Character.'],
-        required: [true, 'please enter your password'],
-        select: false
-    },
-
-    passwordConfirm: {
-        type: String,
-        require: [true, 'please confirm your password'],
-        validate: {
-            validator: function (el) {
-                return el === this.password
-            },
-            message: 'Password didnt match'
-        },
-    },
-
-    passwordChangedAt: Date,
-
-    passwordResetToken: String,
-    passwordResetExpires: Date
-
-
+  passwordResetToken: String,
+  passwordResetExpires: Date
 });
 
-userSchema.pre('save', async function () {
-    if (!this.isModified('password')) return;
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
-    this.password = await bcrypt.hash(this.password, 12);
+  this.password = await bcrypt.hash(this.password, 12);
 
-    this.passwordConfirm = undefined;
+  this.passwordConfirm = undefined;
 
-    this.passwordChangedAt = Date.now() - 1000;
-})
+  this.passwordChangedAt = Date.now() - 1000;
+});
 
 userSchema.methods.checkCorrectPassword = async function (
-    candidatePassword, userPassword
+  candidatePassword,
+  userPassword
 ) {
-    return await bcrypt.compare(candidatePassword, userPassword)
-}
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
-
-
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
