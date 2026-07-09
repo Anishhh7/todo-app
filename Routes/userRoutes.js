7b60313b5a3f229f422910bbbc74e674f3cd7e5e
Anishh <pokharelanish099@gmail.com>
2026-07-09 14:00:12 +0545

    update me function and route added

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,19 +1,13 @@
 const express = require("express");
 const userController = require("./../controller/userController");
 const { model } = require("mongoose");
-const authController= require('./../controller/authController')
+const authController = require("./../controller/authController");
 
 const router = express.Router();
 
-router.post('/signup', authController.signUp);
-router.post('/signin', authController.logIn);
+router.post("/signup", authController.signUp);
+router.post("/login", authController.logIn);
 
-
-// router.route (authController.protect)
-
-router
-    .route("/")
-    .get(userController.getAllUsers)
-
+router.patch("/me", authController.protect, userController.updateMe);
 
 module.exports = router;
--- controller/userController.js
+++ controller/userController.js
@@ -2,7 +2,13 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
-
+const filterObj = (obj, ...allowFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach((el) => {
+    if (allowFields.includes(el)) newObj[el] = obj[el];
+    })
+    return newObj;
+}
 exports.getAllUsers = catchAsync(async (req, res, next) => {
     const users = await User.find();
 
@@ -30,6 +36,23 @@ exports.getUser = catchAsync(async (req, res, next) => {
     })
 });
 
-// exports.createUsers = (req, res, next) => {
-// }
+exports.updateMe = catchAsync(async (req, res, next) => {
+    
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(new AppError('you cannot change password from here', 400));
+    }
+    const filterBody = filterObj(req.body, 'name', 'email');
+    const updateUser = await User.findByIdAndUpdate(
+        req.user.id,
+        filterBody,
+        { returnDocument: 'after', runValidators: true }
+    );
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            user:updateUser
+        }
+    })
 
+
+})
