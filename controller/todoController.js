cbf4ca2a5726b9ac228d6d6293838111a7db1870
Anishh <pokharelanish099@gmail.com>
2026-07-09 13:58:31 +0545

    added stats function

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,24 +1,33 @@
 const Todo = require("../models/todoModel");
 const User = require("../models/userModel");
-const APIFeatures= require('./../utils/apiFeatures')
+const APIFeatures = require("./../utils/apiFeatures");
 const AppError = require("./../utils/appError");
 const catchAsync = require("./../utils/catchAsync");
 
 exports.getAllTodos = catchAsync(async (req, res, next) => {
-  const features = new APIFeatures
-    (Todo.find({ user: req.user.id }), req.query)
+  const features = new APIFeatures(Todo.find({ user: req.user.id }), req.query)
     .filter()
-    .search();
-  
-  const todo = await features.query;
+    .search()
+    .sort()
+    .paginate();
 
+  const todo = await features.query;
+  const total = await Todo.countDocuments({
+    user: req.user.id,
+    ...features.filterConditions,
+  });
+
+  const limit = req.query.limit * 1 || 100;
+  const page = req.query.page * 1 || 1;
+  const totalPages = Math.ceil(total / limit);
 
   res.status(200).json({
     status: "success",
     results: todo.length,
-    data: {
-      todo
-    }
+    total,
+    page,
+    totalPages,
+    data: todo
   });
 });
 
@@ -34,9 +43,9 @@ exports.getTodo = catchAsync(async (req, res, next) => {
 });
 
 exports.createTodos = catchAsync(async (req, res, next) => {
-const newTodo = await Todo.create({
+  const newTodo = await Todo.create({
     ...req.body,
-    user: req.user._id
+    user: req.user.id
   });
 
   res.status(201).json({
@@ -49,7 +58,7 @@ const newTodo = await Todo.create({
 });
 
 exports.updateTodos = catchAsync(async (req, res, next) => {
-  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body ,{
+  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true
   });
@@ -65,8 +74,8 @@ exports.updateTodos = catchAsync(async (req, res, next) => {
 
 exports.deleteTodos = catchAsync(async (req, res, next) => {
   const todo = await Todo.findByIdAndDelete({ _id: req.params.id });
-  
-    if (!todo) {
+
+  if (!todo) {
     return next(new AppError("Todo not found", 404));
   }
 
@@ -78,7 +87,7 @@ exports.deleteTodos = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteMany = catchAsync(async (req, res, next) => {
-  await Todo.deleteMany({user: req.user.id});
+  await Todo.deleteMany({ user: req.user.id });
 
   res.status(204).json({
     status: "success",
@@ -87,6 +96,24 @@ exports.deleteMany = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.dashboardStats = catchAsync(async (req, res, next) => {
-  
-})
+exports.getStats = catchAsync(async (req, res, next) => {
+  const userId = req.user.id;
+
+  const [total, completed, pending, overdue] = await Promise.all([
+    Todo.countDocuments({ user: userId }),
+    Todo.countDocuments({ user: userId, completed:true }),
+    Todo.countDocuments({ user: userId, completed:false}),
+    Todo.countDocuments({
+      user: userId,
+      completed: false,
+      dueDate:{$lt: new Date()}
+    })
+  ])
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      total, completed, pending, overdue
+    }
+  })
+});
