ed7c106faa390885ec7da2d307fc80e9d19bbc95
Anishh <pokharelanish099@gmail.com>
2026-07-09 13:57:10 +0545

    redefined filter and search and added sort and pagination

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,31 +1,54 @@
 class APIFeatures {
-    constructor(query, queryString) {
-        this.query = query;
-        this.queryString = queryString;
-    }
-    
-    filter() {
-        const queryObj = { ...this.queryString };
-        const excludedFields = ['page', 'sort', 'limit', 'fields']
-        excludedFields.forEach(el => delete queryObj[el]);
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+    this.filterConditions = {};
+  }
 
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gt|gte|lte|lt)\b/g, match => `$${match}`);
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ["page", "sort", "limit", "fields", "search"];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
-        this.query = this.query.find(JSON.parse(queryStr));
-        return this;
-    }
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gt|gte|lte|lt)\b/g, (match) => `$${match}`);
 
-    search() {
-        if (this.queryString.search) {
-            this.query = this.query.find({
-                title: {
-                    $regex: this.queryString.search,
-                    $options: "i"}
-            });
+    const parsed = JSON.parse(queryStr);
+    this.filterConditions = { ...this.filterConditions, ...parsed };
+    this.query = this.query.find(parsed);
+    return this;
+  }
+
+  search() {
+    if (this.queryString.search) {
+      const searchCondition = {
+        title: {
+          $regex: this.queryString.search,
+          $options: "i"
         }
-        return this
+      };
+      this.filterConditions = { ...this.filterConditions, ...searchCondition };
+      this.query = this.query.find(searchCondition);
     }
-    
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+    return this;
+  }
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
 }
 module.exports = APIFeatures;
